7a3695ff8099622c522f0a12e9451a0ba7393b0d
Soft <Soft@POSTE-P53>
2025-10-01 10:14:14 +0200

    Add project files.

---
+++ Source/RevisionP2/RevisionP2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RevisionP2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RevisionP2, "RevisionP2" );
